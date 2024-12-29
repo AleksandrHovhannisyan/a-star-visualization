2b09d0e8898b59138c199b0fef693994a44e1931
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-12-29 13:14:08 -0600

    Clean up code and use custom html attributes for demo

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
 import Canvas from "./Canvas";
 import Vector2, { getDistance } from "./Vector2";
 
-const CANVAS_WIDTH = 500;
-const CANVAS_HEIGHT = 500;
-const NUM_COLS = 20;
-const NUM_ROWS = 20;
-const NODE_WIDTH = CANVAS_WIDTH / NUM_COLS;
-const NODE_HEIGHT = CANVAS_HEIGHT / NUM_ROWS;
-
 type Grid = Node[][];
 
 class Node {
+  private width: number;
+  private height: number;
   /** The row-col position of the node in the grid. */
   public position: Vector2;
   public cost: number;
   public gScore: number;
   public neighbors: Node[];
 
-  constructor(position: Vector2) {
-    this.neighbors = [];
+  constructor(position: Vector2, width: number, height: number) {
+    this.width = width;
+    this.height = height;
     this.position = position;
+    this.neighbors = [];
     this.cost = 0;
     this.gScore = 0;
   }
@@ -33,29 +30,24 @@ class Node {
     this.neighbors.push(grid[rowIndex - 1]?.[colIndex]);
     this.neighbors.push(grid[rowIndex + 1]?.[colIndex]);
     this.neighbors = this.neighbors.filter((node) => !!node);
-    // console.log(
-    //   `[${this.position.y}, ${
-    //     this.position.x
-    //   }] has neighbors at: ${this.neighbors
-    //     .map((neighbor) => `[${neighbor.position.y}, ${neighbor.position.x}]`)
-    //     .join(", ")}`
-    // );
   }
 
-  draw(canvas: Canvas, colors?: { fill?: string, stroke?: string }) {
+  draw(canvas: Canvas, colors?: { fill?: string; stroke?: string }) {
     canvas.rect({
-      x: this.position.x * NODE_WIDTH,
-      y: this.position.y * NODE_HEIGHT,
-      width: NODE_WIDTH,
-      height: NODE_HEIGHT,
+      x: this.position.x * this.width,
+      y: this.position.y * this.height,
+      width: this.width,
+      height: this.height,
       fill: colors?.fill,
-      stroke: colors?.stroke ?? 'black',
+      stroke: colors?.stroke,
     });
   }
 }
 
 class Demo extends HTMLElement {
-  static observedAttributes = ["max-fps"];
+  static observedAttributes = ["width", "height", "rows", "cols"];
+  private numRows: number;
+  private numCols: number;
   private canvas: Canvas;
   /** The start position of the search. */
   private start: Vector2;
@@ -68,15 +60,21 @@ class Demo extends HTMLElement {
 
   constructor() {
     super();
+
+    this.numRows = Number(this.getAttribute("rows"));
+    this.numCols = Number(this.getAttribute("cols"));
+
     this.start = new Vector2(0, 0);
-    this.end = new Vector2(NUM_COLS - 1, NUM_ROWS - 1);
+    this.end = new Vector2(this.numCols - 1, this.numRows - 1);
     this.nodes = [];
     // Initialize grid
-    for (let row = 0; row < NUM_ROWS; row++) {
+    const nodeWidth = this.width / this.numCols;
+    const nodeHeight = this.height / this.numRows;
+    for (let row = 0; row < this.numRows; row++) {
       this.nodes[row] = [];
-      for (let col = 0; col < NUM_COLS; col++) {
+      for (let col = 0; col < this.numCols; col++) {
         const nodePosition = new Vector2(col, row);
-        this.nodes[row][col] = new Node(nodePosition);
+        this.nodes[row][col] = new Node(nodePosition, nodeWidth, nodeHeight);
       }
     }
     // Track neighbors for each node in the grid
@@ -89,8 +87,8 @@ class Demo extends HTMLElement {
     const canvas = document.createElement("canvas");
     canvas.style.border = "solid 1px";
     canvas.style.maxWidth = "100%";
-    canvas.width = Number(CANVAS_WIDTH);
-    canvas.height = Number(CANVAS_HEIGHT);
+    canvas.width = this.width;
+    canvas.height = this.height;
     this.canvas = new Canvas(canvas);
     const label = this.getAttribute("title");
     this.removeAttribute("title");
@@ -103,11 +101,42 @@ class Demo extends HTMLElement {
     this.update();
   }
 
+  public get width() {
+    return Number(this.getAttribute("width"));
+  }
+
+  public get height() {
+    return Number(this.getAttribute("height"));
+  }
+
+  private getNodeStroke(node: Node) {
+    return node === this.nodes[0][0] ||
+      node === this.nodes[this.numRows - 1][this.numCols - 1]
+      ? "red"
+      : "black";
+  }
+
   private draw() {
     this.canvas.clear("white");
-    this.nodes.forEach((row) => row.forEach((node) => node.draw(this.canvas, { stroke: node === this.nodes[0][0] || node === this.nodes[NUM_ROWS - 1][NUM_COLS - 1] ? 'red' : 'black' })));
-    this.candidateNodes.forEach((node) => node.draw(this.canvas, { fill: "#90afe0", stroke: node === this.nodes[0][0] || node === this.nodes[NUM_ROWS - 1][NUM_COLS - 1] ? 'red' : 'black' }));
-    this.evaluatedNodes.forEach((node) => node.draw(this.canvas, { fill: "#9be090", stroke: node === this.nodes[0][0] || node === this.nodes[NUM_ROWS - 1][NUM_COLS - 1] ? 'red' : 'black' }));
+    this.nodes.forEach((row) =>
+      row.forEach((node) =>
+        node.draw(this.canvas, {
+          stroke: this.getNodeStroke(node),
+        })
+      )
+    );
+    this.candidateNodes.forEach((node) =>
+      node.draw(this.canvas, {
+        fill: "#90afe0",
+        stroke: this.getNodeStroke(node),
+      })
+    );
+    this.evaluatedNodes.forEach((node) =>
+      node.draw(this.canvas, {
+        fill: "#9be090",
+        stroke: this.getNodeStroke(node),
+      })
+    );
   }
 
   private step() {
@@ -126,12 +155,6 @@ class Demo extends HTMLElement {
         console.log("DONE");
         return;
       }
-
-      // console.log({
-      //   cheapestNodeToVisit,
-      //   candidateNodes: this.candidateNodes,
-      //   evaluatedNodes: this.evaluatedNodes,
-      // });
 
       cheapestNodeToVisit.neighbors.forEach((neighbor) => {
         if (this.evaluatedNodes.has(neighbor)) {
